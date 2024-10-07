aadc57db895000858b02895c152e503c404de46a
jacobtimlin <jacobtimlin23@gmail.com>
2024-10-06 23:56:49 -0500

    Added navbar css to make it look like MU colors, added login and register pages with input fields for email and password, but no functionality yet

--- app.py
+++ app.py
@@ -16,4 +16,15 @@ def about():
     
     return render_template('about.html')
 
+@app.route('/register/')
+def register():
+
+    return render_template('register.html')
+
+@app.route('/login/')
+def login():
+
+    return render_template('login.html')
+
+
 app.run(host="0.0.0.0", port=5002)
